e46fb75af49da1ab832a9ced7d96545fe47a8a2b
Saikishore Gowrishankar <saikishoreg01@gmail.com>
2021-06-03 20:50:29 -0400

    Document rest of code

--- DailyQA.cpp
+++ DailyQA.cpp
@@ -1,23 +1,92 @@
+/****************************************
+ * DailyQA.cpp
+ *
+ * Uses the spreadsheet parser to format
+ * the DailyQA sheet
+ *
+ ****************************************/
+
+/* Standard dependencies */
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+
+/* Local Dependencies */
 #include "spreadsheet.h"
 
-#include <string_view>
-
+/**
+ * DailyQA
+ *
+ * @class This class parses and formats the URL spinner daily report
+ * and outputs the correctly formatted data so that the click rates
+ * can easily be copy/pasted into the daily QA sheet.
+ */
 class DailyQA
 {
 public:
-    DailyQA(std::string_view filename) : sheet{filename.data()}
+    /**
+     * DailyQA()
+     *
+     * @param names File name of sheet with projects/companies
+     */
+    DailyQA(std::string_view names, std::string_view data)
+             : names_sheet{names.data()}, data_sheet{data.data()}
     {
-        for(auto&& line : sheet) data_entries.insert({line[1], line});
+        // Insert data into a hash map for easy lookup
+        for(auto&& line : data_sheet) data_entries.insert({line[0]+line[1], line});
+    }
+
+    /**
+     * run()
+     *
+     * @brief Runs the main program and outputs into the outfile.
+     */
+    void run()
+    {
+        std::string cached_company_name;
+        for(auto&& line : names_sheet)
+        {
+            auto company = line[0], project = line[1];
+
+            // The projects don't all have a company name listed next to them
+            // For example, if the company has multiple carriers it only lists the
+            // name next to the first project. This caches that name so that all
+            // projects under the same company have a company name listed when 
+            // the data is entered into the output file.
+            if(company != "") cached_company_name = company;
+            else if(company == "" && project != "") company = cached_company_name;
+            else if(company == "" && project == "") { outfile << '\n'; continue; }
+
+            // The lookup uses the concatenation of the company and the project names
+            auto str = company+project;
+            if(auto search = data_entries.find(str); search != std::end(data_entries) && search->first != "")
+            {
+                #define out(x,y) ( (x)[y] == ""? "n/a" : (x)[y]  )
+                outfile << out(search->second,0) << ',' << "->" << out(search->second,1) << ','
+                        << out(search->second,4) << ',' << out(search->second,6) << ','<< out(search->second,8) << ',' << out(search->second,10) << "\n";
+                #undef out
+            }
+            else
+            {
+                std::cout << "Could not find $^" << company << " - " << project << "^$\n";
+                outfile << company << ',' << project << ',' << "ERR,ERR,ERR,ERR\n";
+            }
+        }
     }
 private:
-    Spreadsheet sheet;
-    std::unordered_map<std::string,Line> data_entries;
+    Spreadsheet names_sheet, data_sheet; //For the names and data
+    std::unordered_map<std::string, Line> data_entries{}; //Stores data entries
+    std::ofstream outfile{"output.csv"}; //Output file
 };
 
 int main()
 {
-    Spreadsheet doc{"input.csv"};
+    std::cout << "Daily QA Sheet generator\n"
+              << "opening names.csv and data.csv\n\n";
 
-    for(auto&& a : doc) std::cout << a << '\n';
+    DailyQA doc{"names.csv", "data.csv"};
+    doc.run();
+
     return 0;
 }
--- spreadsheet.h
+++ spreadsheet.h
@@ -1,15 +1,18 @@
-/*
-    Spreadsheet Parser (spreadsheet.cpp)
-
-    Author: Saikishore G.
-    Date: 05/26/2021
-
-    All trademarks belong to their respective owners. Lawyers love tautologies.
-*/
+/******************************************************************************
+ *  Spreadsheet Parser (spreadsheet.cpp)
+ *
+ *  Author: Saikishore G.
+ *  Date: 05/26/2021
+ *
+ *  All trademarks belong to their respective owners. Lawyers love tautologies.
+ *
+ *  This is a lightweight spreadsheet (CSV) parser for use with the DailyQA
+ *  program.
+ ******************************************************************************/
 
 #pragma once
 
-//Includes
+/* Standard dependencies */
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -17,30 +20,55 @@
 #include <string_view>
 #include <sstream>
 #include <algorithm>
-#include <iterator>
+#include <cstdlib>
 
 using cell = std::string;
 class Spreadsheet;
 
-/*
-    Line
-
-    Represents the interface for a line in the spreadsheet
-*/
+/******************************************************************************
+ *  Line
+ *
+ *  @class Represents the interface for a line in the spreadsheet
+ ******************************************************************************/
 class Line
 {
 public:
+
+    /* Implementation details*/
     friend class Spreadsheet;
-    using iterator = std::vector<cell>::iterator;
+    using       iterator = std::vector<cell>::      iterator;
     using const_iterator = std::vector<cell>::const_iterator;
 
+    /**
+     * Line()
+     *
+     * @brief Extracts cells from raw line
+     * @param raw Raw line data
+     */
     Line(cell const& raw)
     {
+        using namespace std::string_literals;
+
         std::istringstream ss{raw};
         cell c;
+        auto str = ss.str();
+        if(str[0] == '"')
+        {
+            // @todo Fix output of names w/ comma
+            ss.get();
+            std::getline(ss, c, '"');
+            cells.push_back("\""s+c+"\""s);
+            ss.get();
+        }
         while(std::getline(ss, c, ',')) cells.push_back(c);
     }
 
+    /**
+     * to_raw()
+     *
+     * @brief Extracts cells from raw line
+     * @return std::string holding raw data
+     */
     cell to_raw() const
     {
         std::ostringstream ss;
@@ -50,46 +78,72 @@ public:
         return val;
     }
 
+    /**
+     * ::operator<<()
+     *
+     * @brief Output formatted line
+     */
     friend std::ostream& operator<<(std::ostream& os, Line const& l)
     {
         std::copy(std::begin(l.cells), std::end(l.cells), std::ostream_iterator<cell>(os, " "));
         return os;
     }
 
-    //Iterator methods for range-based for
+    /**
+     * @brief Iterator methods for range-based for
+     */
     iterator       begin()  { return std::begin(cells);  }
     iterator       end()    { return std::end(cells);    }
     const_iterator cbegin() { return std::cbegin(cells); }
     const_iterator cend()   { return std::cend(cells);   }
 
-    cell operator[] (std::ptrdiff_t off) { return cells[off]; }
+    /**
+     * operator[]()
+     *
+     * @brief Access individual cells
+     */
+    cell&       operator[] (std::ptrdiff_t off)         { return cells[off]; }
+    cell const& operator[] (std::ptrdiff_t off) const   { return cells[off]; }
 
 
 private:
+    /* Parsed cells */
     std::vector<cell> cells{};
 };
 
-/*
-    Spreadsheet
-
-    Defines the interface for a spreadsheet parser
-*/
+/******************************************************************************
+ *  Spreadsheet
+ *
+ *  @class Defines the interface for a spreadsheet parser
+ ******************************************************************************/
 class Spreadsheet
 {
 public:
 
-    using iterator = std::vector<Line>::iterator;
+    /* Implementation details*/
+    using iterator       = std::vector<Line>::      iterator;
     using const_iterator = std::vector<Line>::const_iterator;
 
+    /**
+     * Spreadsheet()
+     *
+     * @param in The input file name
+     *
+     **/
     Spreadsheet(std::string_view in) : infile{in.data()}
     {
-        if(!infile) return;
+        if(!infile) { std::cerr << "could not open file " << in.data() << ". Abort.\n"; std::exit(1); }
 
         std::string raw_line{};
-        while(std::getline(infile, raw_line)) {
-            lines.push_back(raw_line);}
+        while(std::getline(infile, raw_line)) lines.push_back(raw_line);
     }
 
+    /**
+     * ::operator<< ()
+     *
+     * @brief Outputs formatted spreadsheet
+     *
+     **/
     friend std::ostream& operator<<(std::ostream& os, Spreadsheet const& s)
     {
         for(auto&& l : s.lines) os << l << '\n';
@@ -103,6 +157,6 @@ public:
     const_iterator cend()   { return std::cend(lines);   }
 
 private:
-    std::ifstream infile;
-    std::vector<Line> lines{};
+    std::ifstream infile;	//Input file
+    std::vector<Line> lines{};	//Parsed lines
 };
